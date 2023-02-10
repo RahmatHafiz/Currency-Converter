e4ced420503b871d82e90bd3b8fbd529b8094e34
Rahmat Hafiz J <rahmat.hafiz.jayadiningrat@students.uin-suska.ac.id>
2023-02-10 18:02:53 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,62 @@
+const dropList = document.querySelectorAll("form select"),
+    fromCurrency = document.querySelector(".from select"),
+    toCurrency = document.querySelector(".to select"),
+    getButton = document.querySelector("form button");
+
+for (let i = 0; i < dropList.length; i++) {
+    for (let currency_code in daftar_negara) {
+        let selected = i == 0 ? currency_code == "IDR" ? "selected" : "" : currency_code == "MYR" ? "selected" : "";
+        let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
+        dropList[i].insertAdjacentHTML("beforeend", optionTag);
+    }
+    dropList[i].addEventListener("change", e => {
+        loadFlag(e.target);
+    });
+}
+
+function loadFlag(element) {
+    for (let code in daftar_negara) {
+        if (code == element.value) {
+            let imgTag = element.parentElement.querySelector("img");
+            imgTag.src = `https://flagcdn.com/48x36/${daftar_negara[code].toLowerCase()}.png`;
+        }
+    }
+}
+
+window.addEventListener("load", () => {
+    getExchangeRate();
+});
+
+getButton.addEventListener("click", e => {
+    e.preventDefault();
+    getExchangeRate();
+});
+
+const exchangeIcon = document.querySelector("form .icon");
+exchangeIcon.addEventListener("click", () => {
+    let tempCode = fromCurrency.value;
+    fromCurrency.value = toCurrency.value;
+    toCurrency.value = tempCode;
+    loadFlag(fromCurrency);
+    loadFlag(toCurrency);
+    getExchangeRate();
+})
+
+function getExchangeRate() {
+    const amount = document.querySelector("form input");
+    const exchangeRateTxt = document.querySelector("form .exchange-rate");
+    let amountVal = amount.value;
+    if (amountVal == "" || amountVal == "0") {
+        amount.value = "1";
+        amountVal = 1;
+    }
+    exchangeRateTxt.innerText = "Getting exchange rate...";
+    let url = `https://v6.exchangerate-api.com/v6/dcc0e23b9763410cb5e94bd9/latest/${fromCurrency.value}`;
+    fetch(url).then(response => response.json()).then(result => {
+        let exchangeRate = result.conversion_rates[toCurrency.value];
+        let totalExRate = (amountVal * exchangeRate).toFixed(2);
+        exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExRate} ${toCurrency.value}`;
+    }).catch(() => {
+        exchangeRateTxt.innerText = "Something went wrong";
+    });
+}
